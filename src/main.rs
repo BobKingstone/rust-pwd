c70a1f03b8729d43970a224c7ac5fb4e1604b17f
bobkingstone <robert.kingstone@gmail.com>
2015-08-01 19:22:07 +0100

    Added ability to view stored strings

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::OpenOptions;
 use std::io;
 use std::fs;
 
+extern crate crypto;
 
 /*
  *
@@ -19,30 +20,80 @@ fn write_to_file(filename: &str) -> io::Result<()> {
          .write(true)  //has write to file permission
          .append(true) //append buffer to end of file
          .open(name)); //open file with name 'name'
-         
 
-     try!(writeln!(file, "Bob was here twice"));
+     let mut record_ref = String::new();
+     let mut record_content = String::new();
+
+     println!("enter a password ref:\n");
+
+     io::stdin().read_line(&mut record_ref)
+            .ok()
+            .expect("Failed to read your selection");
+
+     println!("enter a password:\n");
+
+     io::stdin().read_line(&mut record_content)
+            .ok()
+            .expect("Failed to read your selection");
+
+     let new_record = format!(":{} - {}",record_ref,record_content);
+         
+     try!(writeln!(file,"{}",new_record));
      
      return Ok(());
 }
 
 
+/*
+ *  read named file, displaying any matching strings
+ *  
+ *  params String filename
+ *
+ */
 fn read_from_file(filename: &str) -> io::Result<()> {
+    
     let name = filename.to_string() + ".txt";
+    let mut record = String::new();
+    let mut file_contents = String::new();
+
     let mut file = try!(OpenOptions::new()
                         .create(true)
                         .read(true)
                         .open(name));
 
-    let mut file_contents = String::new();
     
     try!(Read::read_to_string(&mut file, &mut file_contents));
+    
+    let mut split: Vec<&str> = file_contents.split(":").collect();
 
-    println!("{}",file_contents);
+    // get user defined record to find
+    println!("Enter record name to retrieve: ");
+        
+    io::stdin().read_line(&mut record)
+            .ok()
+            .expect("Failed to read your selection");
+    
+    let record_substr = &record.trim();
+
+    for s in split {
+        if s.contains(record_substr) {
+            println!("{}",s.trim());
+        }
+    }
 
     return Ok(());
 }
 
+
+/*
+ *
+ * delete file
+ *
+ * param String name
+ *
+ * return Rssult
+ *
+ */
 fn delete_file(filename: &str) -> io::Result<()> {
     let name = filename.to_string() + ".txt";
 
@@ -60,28 +111,51 @@ fn print_error(error: &str) {
     println!("There was an error {} to the file", error);
 }
 
+
+
 fn main() {
+    
+    let filename = "name";
     //create/open file
     //list choices
     //  a) write string
     //  b) read from file
     //  c) delete file
+    let mut selection = String::new();
+    
+    loop {
+        println!("Please make a selection.\na) Store new password\nb) read stored password\nc) delete password file\nq) Quit PWD");
 
-    let filename = "name";
+        io::stdin().read_line(&mut selection)
+            .ok()
+            .expect("Failed to read your selection");
 
-    let write = write_to_file(&filename);
-
-    //check for errors when writing to file
-    if write.is_err() {
-        print_error("writing");
+        //check for matching selection
+        match selection.trim() {
+            "a" => { 
+                let write = write_to_file(&filename);
+                if write.is_err() {
+                    print_error("writing");
+                }
+                
+            },
+            "b" => {
+                let read = read_from_file(&filename);
+                if read.is_err() {
+                    println!("There was an error tryig to read the file, have you created on?");
+                }
+            }
+            "c" => { 
+                let _delete = delete_file(filename);
+            },
+            "q" => {
+                println!("Goodbye");
+                break;
+            },
+            _ => selection = "".to_string(),
+        };
+        //reset selection - will cause compiler warning
+        selection = "".to_string(); 
     }
-
-    let read = read_from_file(&filename);
-
-    if read.is_err() {
-        print_error("reading");
-    }
-
-    let _delete = delete_file(filename);
-
+ 
 }
